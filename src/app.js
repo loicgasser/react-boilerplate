62fcaa4cfb6c7e9e452b9d3e55648bf01c206318
loicgasser <loicgasser4@gmail.com>
2018-09-14 17:27:16 -0400

    Connect app to the store

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
 import Default from './components/Default'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-ReactDOM.render(<Default />, document.getElementById('app'))
+const store = configureStore()
+
+const jsx = (
+    <Provider store={store}>
+        <Default />
+    </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
